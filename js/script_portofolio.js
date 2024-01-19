ae466780988a67390f997f75a21985287f795ce5
SkandarEverest <skandar.everest@gmail.com>
2024-01-19 16:46:52 +0700

    first commit

---
+++ js/script_portofolio.js
@@ -0,0 +1,41 @@
+const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      // let portfolioIsotope = new Isotope(portfolioContainer, {
+      //   itemSelector: '.portfolio-item'
+      // });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+        this.getAttribute('data-filter')
+
+      }, true);
+    }
+
+  });
---
+++ js/script_todo.js
@@ -0,0 +1,82 @@
+function displayTodo(){
+    const todos = JSON.parse(localStorage.getItem('todos'));
+    
+    let list = "";
+
+    if(todos){
+        for (let i = 0; i < todos.length; i++) {
+            list+= `
+                <ul class="list-group list-group-horizontal rounded-0 bg-transparent m-2">
+                    <li class="list-group-item d-flex align-items-center ps-0 pe-3 py-1 rounded-0 border-0 bg-transparent">
+                        <div class="form-check">
+                            <input class="form-check-input me-0" type="checkbox" value="" id="${todos[i].id}" onchange="setComplete(this.checked, this.id)" ${todos[i].checked ? 'checked' : ''}>
+                        </div>
+                    </li>
+                    <li class="list-group-item px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent">
+                        <p class="lead fw-normal mb-0" style="${todos[i].checked ? 'text-decoration: line-through;' : ''}">${todos[i].name}</p>
+                    </li>
+                    <li class="list-group-item px-3 py-1 d-flex align-items-center border-0 bg-transparent">
+                        <button type="button" class="btn btn-danger" id="${todos[i].id}" onclick="deleteTodo(this.id)">Delete</button>
+                    </li>
+                </ul>
+            `;
+        }
+    } 
+
+    document.getElementById('list-todo').innerHTML = list;
+}
+
+function submitTodo(){
+    const todo = document.getElementById('add-todo');
+
+    let todos = JSON.parse(localStorage.getItem('todos'));
+
+    if(todos){
+        todos.push({
+            id: todos[todos.length-1].id + 1,
+            name: todo.value,
+            checked: false
+        });
+    } else {
+        todos = [{
+            id: 0,
+            name: todo.value,
+            checked: false
+        }];
+    }
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+    todo.value = '';
+    displayTodo();
+}
+
+function setComplete(checked, id){
+    let todos = JSON.parse(localStorage.getItem('todos'));
+
+    todos.map(el => {
+        if (el.id === Number(id)){
+            el.checked = checked;
+        }
+        return el;
+    });
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+    displayTodo();
+}
+
+function deleteTodo(id){
+    let todos = JSON.parse(localStorage.getItem('todos'));
+    
+    todos = todos.filter(el => {
+        return el.id !== Number(id);
+    });
+    
+    if (todos.length > 0) {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    } else {
+        localStorage.removeItem('todos');
+    }
+
+    displayTodo();
+}
+
